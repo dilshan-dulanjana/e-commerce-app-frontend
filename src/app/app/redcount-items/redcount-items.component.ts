29716a70b4227f6b8f18bce35eccc8adc3891c15
DDG000 <dilshanguruge99930@gmail.com>
2024-12-23 11:39:44 +0530

    red count item is added

--- src/app/app/redcount-items/redcount-items.component.ts
+++ src/app/app/redcount-items/redcount-items.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-redcount-items',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './redcount-items.component.html',
   styleUrl: './redcount-items.component.css'
 })
-export class RedcountItemsComponent {
+export class RedcountItemsComponent implements OnInit {
+
+  constructor(private http:HttpClient) {}
+  ngOnInit(): void {
+    this.getRedCardItems()
+  }
+
+  cards: any[] = [];
+
+  getRedCardItems() {
+    this.http.get<any>("http://localhost:8080/getRedCount").subscribe(
+      (res) => {
+        console.log(res);
+       
+        if (res && res.Red_Count_Products) {
+          this.cards = res.Red_Count_Products;
+        }
+      },
+      (error) => {
+       alert("There is error...!!!");
+      }
+    );
+  }
 
 }
